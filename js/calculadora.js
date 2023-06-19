b71b6dde9ce3fb6eb7a9ca89d20effd27c699679
SVHugo <hugosvalencia7@gmail.com>
2023-06-19 11:50:41 -0500

    repo de calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,27 @@
+const display = document.querySelector(".resultado");
+const botones = document.querySelectorAll("button");
+let num;
+let num2 = 2;
+let resultado = 0;
+let operador;
+botones.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(button.textContent);
+
+    if (button.textContent === "=") {
+      operacion();
+      return;
+    }
+    displayPring(button.textContent);
+  });
+});
+
+const displayPring = (n) => {
+  display.innerHTML += n;
+};
+
+const operacion = () => {
+  let x = display.innerHTML;
+  console.log("=>", x);
+  display.innerHTML = eval(x);
+};
